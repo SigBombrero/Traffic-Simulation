76b2e14fc5651deb718a446feaa4fe39c1d4812e
Edrees Osman <34333863+SigBombrero@users.noreply.github.com>
2018-03-09 19:40:59 -0800

    Update TrafficTest.java

--- TrafficTest.java
+++ TrafficTest.java
@@ -2,15 +2,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.Timer;
+import javax.swing.*;
 
 public class TrafficTest {
 	
 	private static int DELAY = 100;
-	private static int counter = 0;	
+	private static int counter = 0;
+
+	public static ActionListener changeSpeed(JTextField tf, Car c) {
+		return e -> {
+			String s = tf.getText();
+			c.setSpeed(Integer.parseInt(s), 0);
+		} ;
+	}
 	
 	public static void main (String args[]) {
 			
@@ -21,43 +25,34 @@ public class TrafficTest {
 		Road road = new Road(600, 600);
 		Car c1 = new Car(0, 600/2, 40, 20, 0, 50);
 		Car c2 = new Car(600, 600/2-20, 40, -7, 0, -50);
-		
+
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new GridLayout(2, 2));
+
 		JTextField tf1 = new JTextField(20);
 		tf1.setText("Speed of Car1");
-		JButton button1 = new JButton("Change");
+        	JButton button1 = new JButton("Change");
 
-		/*JTextField tf2 = new JTextField(20);
-		tf1.setText("Speed of Car2");
-		JButton button2 = new JButton("Change");*/
+		JTextField tf2 = new JTextField(20);
+		tf2.setText("Speed of Car2");
+        	JButton button2 = new JButton("Change");
 		
-		
-		button1.addActionListener(new ActionListener () {
-			public void actionPerformed (ActionEvent event) {
-				String s = tf1.getText();
-				c1.setSpeed(Integer.parseInt(s), 0);
-			}
-		});
+		button1.addActionListener(TrafficTest.changeSpeed(tf1, c1));
 
-		/*button2.addActionListener(new ActionListener () {
-			public void actionPerformed (ActionEvent event) {
-				String s = tf2.getText();
-				c2.setSpeed(Integer.parseInt(s), 0);
-			}
-		});*/
+        	button2.addActionListener(TrafficTest.changeSpeed(tf2, c2));
+
+        	controlPanel.add(tf1);
+        	controlPanel.add(button1);
+        	controlPanel.add(tf2);
+        	controlPanel.add(button2);
 		
-		road.setLayout(new FlowLayout());
+		window.setLayout(new BorderLayout());
 		road.addCar(c1);
 		road.addCar(c2);
-		
-		road.add(tf1);
-		road.add(button1);
-		// road.add(tf2);
-		// road.add(button2);
-		
-		window.add(road);
-		
-		
-		
+
+		window.add(controlPanel, BorderLayout.NORTH);
+		window.add(road, BorderLayout.CENTER);
+
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		
